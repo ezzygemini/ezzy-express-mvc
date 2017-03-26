74ccbddcaa0035862983ba4c8a066e30ea2b64d4
Moises Romero <moises@novacloudware.com>
2017-03-26 11:10:06 -0700

    Fixing the data injection

--- src/Controller.js
+++ src/Controller.js
@@ -92,7 +92,7 @@ class Controller extends Request {
         const Model = this._model;
         const model = new Model(basics);
         dataPromise = model.getData(basics)
-          .then(model => ({model, [this._modelName]: model}));
+          .then(data => (Object.assign(model, {data})));
       } catch (e) {
         logger.error(e);
         dataPromise = Promise.resolve(basics);
